d702cbe55bb1f0a8fced83441a737df6fef6d838
Rúben França <ruben_tiagof@hotmail.com>
2018-07-30 01:56:12 +0100

    Add pageHeader component

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import PageHeader from '../template/pageHeader';
 
 export default props => (
     <div>
-        <h1>About</h1>
+        <PageHeader name='About' small='Us'></PageHeader>
+
+        <h2>Our story</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+
+        <h2>Our mission</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+
+        <h2>News</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+
     </div>
 );
